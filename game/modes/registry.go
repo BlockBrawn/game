0060c6f3ec08b251c4121d0d5ea7d69878bbe710
Josscoder <josscoder@hotmail.com>
2025-12-24 14:33:29 -0500

    feat: impl mode registry

---
+++ game/modes/registry.go
@@ -0,0 +1,19 @@
+package modes
+
+var registry = map[string]Mode{
+	Normal{}.String(): Normal{},
+	Solo{}.String():   Solo{},
+	Duo{}.String():    Duo{},
+	Squad{}.String():  Squad{},
+}
+
+func RegisterMode(mode Mode) {
+	registry[mode.String()] = mode
+}
+
+func GetModeFromString(mode string) Mode {
+	if m, ok := registry[mode]; ok {
+		return m
+	}
+	return nil
+}
